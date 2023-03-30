344e4a37e50362d0ad32476c8059ef8d7dcc344f
solidSpoon <cedarrival@outlook.com>
2023-03-30 16:52:51 +0800

    Update cedru.rb

--- Formula/cedru.rb
+++ Formula/cedru.rb
@@ -1,8 +1,8 @@
 class Cedru < Formula
   desc "命令行数字排序工具"
   homepage "https://github.com/solidSpoon/cedru"
-  url "https://github.com/myuser/cedru/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "下载文件的 SHA256 值"
+  url "https://github.com/solidSpoon/cedru/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "0ef02a08c6b1cf14b59790740f9707e2a12978c22f6ed2fc852e30070eef087c"
   license "Apache License 2.0"
 
   depends_on "rust" => :build
